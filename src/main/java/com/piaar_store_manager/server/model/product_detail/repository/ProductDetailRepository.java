1b630f97f4d97c80657e6292f0df5441e40822e5
fkakd1205 <fkakd0102@naver.com>
2021-11-11 19:57:03 +0900

    test

--- src/main/java/com/piaar_store_manager/server/model/product_detail/repository/ProductDetailRepository.java
+++ src/main/java/com/piaar_store_manager/server/model/product_detail/repository/ProductDetailRepository.java
@@ -10,4 +10,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProductDetailRepository extends JpaRepository<ProductDetailEntity, Integer> {
     List<ProductDetailEntity> findByProductOptionCid(Integer productOptionCid);
+    // test
 }
