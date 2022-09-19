23ae08fc6beb13ce389241c34839f641f90f9a7b
fkakd1205 <fkakd0102@naver.com>
2022-09-19 10:28:50 +0900

    DOCS : 불필요한 출력코드 제거

--- src/main/java/com/piaar_store_manager/server/domain/erp_order_item/service/ErpOrderItemBusinessService.java
+++ src/main/java/com/piaar_store_manager/server/domain/erp_order_item/service/ErpOrderItemBusinessService.java
@@ -374,7 +374,6 @@ public class ErpOrderItemBusinessService {
                 }
                 if (!duplication) {
                     newItems.add(duplicationCheckItem);
-                    System.out.println(duplicationCheckItem);
                 }
             }
         }
